fd0860114c2567f4a4c9f295412f6e8ef2a44a52
med-integems <m.kamara@integems.com>
2025-09-26 11:33:32 +0000

    modify next.config

--- next.config.ts
+++ next.config.ts
@@ -5,13 +5,8 @@ import { fileURLToPath } from "url";
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
-const projectRoot = __dirname;
 
 const nextConfig: NextConfig = {
-  outputFileTracingRoot: projectRoot,
-  turbopack: {
-    root: projectRoot,
-  },
   eslint: {
     dirs: ["src"],
     ignoreDuringBuilds: true,
